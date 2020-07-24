d4ab0d8eb7602fce2e1fe7fb92ac4b52d673b0cc
vitaly.derbin <vuderbin@gmail.com>
2020-07-24 23:04:35 +0300

    initial

---
+++ BackgroundQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concurrency.BackgroundQueue
+{
+    public class MySyncDomainObject
+    {
+        public void DoSomeStuff(RemoteBackendClient client)
+        {
+            client.CallApi("hello!");
+        }
+    }
+
+    public class RemoteBackendClient : IDisposable
+    {
+        private readonly CancellationTokenSource _cancellation = new CancellationTokenSource();
+        private readonly BlockingCollection<string> _queue = new BlockingCollection<string>();
+
+        public RemoteBackendClient()
+        {        
+            _ = Task.Run(()=>EndlessLoop(_cancellation.Token), _cancellation.Token);
+        }
+
+        private async Task EndlessLoop(CancellationToken token)
+        {
+            while(!_queue.IsCompleted && !token.IsCancellationRequested)
+            {
+                if (_queue.TryTake(out var item, 1000))
+                {
+                    await CallRemoteApi(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// here should be real API call
+        /// </summary>
+        private Task CallRemoteApi(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// just add message to queue
+        /// </summary>
+        public void CallApi(string message)
+        {
+            _queue.Add(message);
+        }
+
+        public void Dispose()
+        {
+            _cancellation.Cancel();
+        }
+    }
+}
---
+++ EventLoop.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concurrency.EventLoop
+{
+    public class SyncHandler : IDisposable
+    {
+        private readonly IDisposable _subscription;
+
+        public SyncHandler(RemoteBackendClient client)
+        {
+            _subscription = client.Inbox.Subscribe(Handle);
+        }
+
+        /// <summary>
+        /// event handling logic should be here
+        /// </summary>
+        private void Handle(MyEvent e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _subscription.Dispose();
+        }
+    }
+
+    public class RemoteBackendClient : IDisposable
+    {
+        private readonly CancellationTokenSource _cancellation = new CancellationTokenSource();
+        public RemoteBackendClient()
+        {
+            _ = Task.Run(() => EndlessLoop(_cancellation.Token), _cancellation.Token);
+        }
+
+        private async Task EndlessLoop(CancellationToken token)
+        {
+            while(!token.IsCancellationRequested)
+            {
+                var @event = await ReceiveData();
+                Inbox.OnNext(@event);
+            }
+        }
+
+        /// <summary>
+        /// receiving logic should be here
+        /// </summary>
+        private Task<MyEvent> ReceiveData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Subject<MyEvent> Inbox { get; } = new Subject<MyEvent>();
+        
+        public void Dispose()
+        {
+            Inbox.OnCompleted();
+            Inbox.Dispose();
+            
+            _cancellation.Cancel();
+        }
+    }
+
+    public class MyEvent
+    {
+    }
+}
